492926c0c5519ff8950db99d0b2ebbf8a1170ac1
misupovwork <misupov_work@kse.org.ua>
2026-06-03 14:15:51 +0300

    Update fetch_donations.py

--- fetch_donations.py
+++ fetch_donations.py
@@ -49,8 +49,9 @@ def get_yesterday_donations(access_token):
     if response.status_code == 204:
         return []
 
+    if response.status_code != 200:
+    print("Zoho search error:", response.status_code, response.text)
     response.raise_for_status()
-    return response.json().get("data", [])
 
 
 def write_to_notion(donor_name, donor_email, amount, date):
